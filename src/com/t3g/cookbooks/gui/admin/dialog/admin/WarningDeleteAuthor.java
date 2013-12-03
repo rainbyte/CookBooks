7ac917a337de9da912a1a9493f89528014df0c7c
Martin Goya <tincho_592@hotmail.com>
2013-12-03 18:12:30 -0300

    Creada WarningDeleteAuthor

---
+++ src/com/t3g/cookbooks/gui/admin/dialog/admin/WarningDeleteAuthor.java
@@ -0,0 +1,86 @@
+package com.t3g.cookbooks.gui.admin.dialog.admin;
+
+import java.awt.Color;
+import java.awt.EventQueue;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+import java.sql.SQLException;
+
+import javax.swing.JButton;
+import javax.swing.JDialog;
+import javax.swing.JPanel;
+import javax.swing.WindowConstants;
+import javax.swing.border.EmptyBorder;
+import javax.swing.border.TitledBorder;
+import javax.swing.border.EtchedBorder;
+import javax.swing.JLabel;
+
+import com.t3g.cookbooks.db.Database;
+
+/**
+ * 
+ * @author T3G
+ */
+public class WarningDeleteAuthor extends JDialog {
+	private static final long serialVersionUID = 1L;
+	private JPanel contentPane;
+	private long id;
+	/**
+	 * Create the frame.
+	 */
+	public WarningDeleteAuthor(long id1) {
+		setTitle("ADVERTENCIA!");
+		setDefaultCloseOperation(WindowConstants.DISPOSE_ON_CLOSE);
+		setBounds(420, 100, 385, 154);
+		contentPane = new JPanel();
+		contentPane.setBackground(new Color(153, 153, 255));
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+		setContentPane(contentPane);
+		contentPane.setLayout(null);
+		id = id1;
+
+		JButton btnBack = new JButton("Volver Atras");
+		btnBack.addMouseListener(new MouseAdapter() {
+			@Override
+			public void mousePressed(MouseEvent arg0) {
+				close();
+			}
+		});
+		btnBack.setBackground(new Color(153, 153, 255));
+		btnBack.setBounds(10, 93, 136, 23);
+		contentPane.add(btnBack);
+
+		JPanel panel = new JPanel();
+		panel.setBorder(new TitledBorder(new EtchedBorder(EtchedBorder.LOWERED, null, null), "Baja de un Autor", TitledBorder.LEADING, TitledBorder.TOP, null, null));
+		panel.setBounds(82, 11, 224, 71);
+		contentPane.add(panel);
+		
+		JLabel lblDebeIniciarSesin = new JLabel("Esta a punto de eliminar un Autor");
+		panel.add(lblDebeIniciarSesin);
+		
+		JLabel lblParaPoderAcceder = new JLabel("\u00BFA\u00FAn desea hacerlo?");
+		panel.add(lblParaPoderAcceder);
+		
+		JButton btnConfirmarBaja = new JButton("Confirmar baja");
+		btnConfirmarBaja.addMouseListener(new MouseAdapter() {
+			@Override
+			public void mousePressed(MouseEvent arg0) {
+				// Remove from db
+				try {
+					Database.getAuthorDao().deleteById(id);
+				} catch (SQLException e) {
+					e.printStackTrace();
+				}
+				close();
+			}
+		});
+		btnConfirmarBaja.setBackground(new Color(153, 153, 255));
+		btnConfirmarBaja.setBounds(231, 93, 136, 23);
+		contentPane.add(btnConfirmarBaja);
+	}
+	
+	private void close() {
+		this.dispose();
+	}
+
+}
