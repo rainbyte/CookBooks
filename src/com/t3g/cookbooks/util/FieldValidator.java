3fb9d69c6280e8768eb3e33077a686262a8f3a7d
Martin Goya <tincho_592@hotmail.com>
2013-12-03 17:56:31 -0300

    Actualizo FieldValidator
    Cambios:
    * Agregado metodo isGender().

--- src/com/t3g/cookbooks/util/FieldValidator.java
+++ src/com/t3g/cookbooks/util/FieldValidator.java
@@ -46,6 +46,9 @@ public class FieldValidator {
 		return input.matches("^[a-zA-Z0-9áíúéóü/\\\\,_-ñ@?¿:·%= ]+(.jpg|.jpeg|.png|.gif|.bpm)$");
 	}
 
+	public static boolean isGender(String input) {
+		return input.matches("^[mMfF]$");
+	}
 	public static void main(String[] args) {
 		String alpha = "aiueo";
 		String alphanumeric = "aiueo12345";
