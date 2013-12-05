914ad019806c84e9e93ef3335aaf0de627ed6867
Martin Goya <tincho_592@hotmail.com>
2013-12-05 11:30:04 -0300

    Actualizada CancelPurchase
    Cambios:
    * Se acomodo un poco la interfaz.
    * Se agrego la funcion "close()" luego de confirmar la cancelacion.

--- src/com/t3g/cookbooks/gui/admin/dialog/user/CancelPurchase.java
+++ src/com/t3g/cookbooks/gui/admin/dialog/user/CancelPurchase.java
@@ -13,6 +13,7 @@ import javax.swing.WindowConstants;
 import javax.swing.border.EmptyBorder;
 
 import com.t3g.cookbooks.gui.abstraction.DataWindow;
+import javax.swing.SwingConstants;
 
 /**
  * 
@@ -34,7 +35,7 @@ public class CancelPurchase extends JDialog {
 		
 		setDefaultCloseOperation(WindowConstants.DISPOSE_ON_CLOSE);
 
-		setBounds(100, 100, 450, 300);
+		setBounds(100, 100, 450, 210);
 		contentPane = new JPanel();
 		contentPane.setBackground(new Color(153, 153, 255));
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
@@ -43,13 +44,17 @@ public class CancelPurchase extends JDialog {
 
 		JLabel label = new JLabel(
 				"Esta a punto de cancelar la compra de los art\u00EDculos");
-		label.setFont(new Font("Tahoma", Font.PLAIN, 13));
-		label.setBounds(79, 60, 297, 14);
+		label.setForeground(new Color(204, 0, 0));
+		label.setHorizontalAlignment(SwingConstants.CENTER);
+		label.setFont(new Font("Tahoma", Font.BOLD, 15));
+		label.setBounds(10, 29, 414, 25);
 		contentPane.add(label);
 
 		JLabel label_1 = new JLabel("que estan en su carrito de compra");
-		label_1.setFont(new Font("Tahoma", Font.PLAIN, 13));
-		label_1.setBounds(117, 85, 197, 14);
+		label_1.setForeground(new Color(204, 0, 0));
+		label_1.setHorizontalAlignment(SwingConstants.CENTER);
+		label_1.setFont(new Font("Tahoma", Font.BOLD, 15));
+		label_1.setBounds(10, 54, 414, 25);
 		contentPane.add(label_1);
 
 		JButton button = new JButton("Volver Atras");
@@ -59,8 +64,8 @@ public class CancelPurchase extends JDialog {
 				close();
 			}
 		});
-		button.setBackground(new Color(153, 153, 255));
-		button.setBounds(51, 175, 137, 23);
+		button.setBackground(Color.RED);
+		button.setBounds(10, 142, 137, 23);
 		contentPane.add(button);
 
 		JButton button_1 = new JButton("Confirmar cancelacion");
@@ -71,12 +76,13 @@ public class CancelPurchase extends JDialog {
 			}
 		});
 		button_1.setBackground(new Color(153, 153, 255));
-		button_1.setBounds(217, 175, 159, 23);
+		button_1.setBounds(210, 142, 214, 23);
 		contentPane.add(button_1);
 	}
 	
 	private void btnCancelPurchaseMousePressed() {
 		dataWindow.deleteData();
+		close();
 	}
 	
 	private void close() {
